f3f963a9f88f233766dc524bb99f3384a8b1c407
sunyue <sunyue@blchina.com>
2022-09-08 15:02:15 +0800

    feat：消除警告

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,15 @@ import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 export default defineConfig({
+	//静态资源服务的文件夹
+	publicDir: "public",
 	base: './',
+	//静态资源处理
+	assetsInclude: "",
+	//控制台输出的级别 info 、warn、error、silent
+	logLevel: "info",
+	// 设为false 可以避免 vite 清屏而错过在终端中打印某些关键信息
+	clearScreen: true,
 	resolve: {                                       
 		alias: {
 			'@': resolve(__dirname, 'src')         
@@ -16,14 +24,34 @@ export default defineConfig({
 		vue(),
 		VueSetupExtend(),
 		AutoImport({
-			resolvers: [ElementPlusResolver()]
+			imports: ["vue"],
+			resolvers: [ElementPlusResolver()],
+			dts: resolve(resolve(__dirname,'src'), "auto-imports.d.ts"),
 		}),
 		Components({
-			resolvers: [ElementPlusResolver()]
-			// dts: false
+			resolvers: [ElementPlusResolver()],
+			dts: resolve(resolve(__dirname,'src'), "components.d.ts")
 		})
 	],
 	optimizeDeps: {
 		include: ['schart.js']
+	},
+	server:{
+		host: '127.0.0.1',
+		//port: Number(loadEnv(mode, process.cwd()).VITE_APP_PORT),
+		port: 3000,
+		strictPort: true, // 端口被占用直接退出
+		https: false,
+		open: false,// 在开发服务器启动时自动在浏览器中打开应用程序
+		proxy: {
+			'^/api': {
+					target: '',
+					changeOrigin: true,
+					rewrite: (path) => path.replace(/^\/api/, '')
+			}
+		},
+		hmr:{
+			overlay: false // 屏蔽服务器报错
+		}
 	}
 });
