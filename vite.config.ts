d0655b9edbde89476661c3cde7bcf3537ba0a9c1
sunyue <sunyue@blchina.com>
2022-09-14 15:47:18 +0800

    feat：增加打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import VueSetupExtend from 'vite-plugin-vue-setup-extend';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import viteCompression from 'vite-plugin-compression'
+import viteImagemin from 'vite-plugin-imagemin'
 export default defineConfig({
 	//静态资源服务的文件夹
 	publicDir: "public",
@@ -33,21 +35,80 @@ export default defineConfig({
 		vue(),
 		VueSetupExtend(),
 		AutoImport({
-			imports: ["vue"],
+			imports: ["vue","vue-router", "pinia"],
 			resolvers: [ElementPlusResolver()],
 			dts: resolve(resolve(__dirname,'src'), "auto-imports.d.ts"),
 		}),
 		Components({
 			resolvers: [ElementPlusResolver()],
 			dts: resolve(resolve(__dirname,'src'), "components.d.ts")
+		}),
+		viteCompression({
+			verbose: true,
+			disable: false,
+			threshold: 10240,
+			algorithm: 'gzip',
+			ext: '.gz',
+		}),
+		viteImagemin({
+			gifsicle: {
+				optimizationLevel: 7,
+				interlaced: false
+			},
+			optipng: {
+				optimizationLevel: 7
+			},
+			mozjpeg: {
+				quality: 20
+			},
+			pngquant: {
+				quality: [0.8, 0.9],
+				speed: 4
+			},
+			svgo: {
+				plugins: [
+					{
+						name: 'removeViewBox'
+					},
+					{
+						name: 'removeEmptyAttrs',
+						active: false
+					}
+				]
+			}
 		})
 	],
 	optimizeDeps: {
 		include: ['schart.js']
 	},
+  build: {
+    outDir: "dist",
+    // 9月更新
+    assetsDir: "assets", //指定静态资源存放路径
+    sourcemap: false, //是否构建source map 文件
+		chunkSizeWarningLimit: 1500,
+    terserOptions: {
+      // 生产环境移除console
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      }
+    },
+		rollupOptions: {
+			output: {
+				chunkFileNames: 'static/js/[name]-[hash].js',
+				entryFileNames: 'static/js/[name]-[hash].js',
+				assetFileNames: 'static/[ext]/[name].[hash].[ext]',
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          }
+        }
+      }
+		}
+	},
 	server:{
 		host: '127.0.0.1',
-		//port: Number(loadEnv(mode, process.cwd()).VITE_APP_PORT),
 		port: 3000,
 		strictPort: true, // 端口被占用直接退出
 		https: false,
